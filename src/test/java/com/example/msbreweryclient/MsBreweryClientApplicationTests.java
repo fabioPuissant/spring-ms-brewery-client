09abe831fc2bac24a847d4ab202c324bce3cdedf
FabioP <fabio.puissant@hotmail.com>
2021-11-24 22:53:29 +0100

    initial commit

---
+++ src/test/java/com/example/msbreweryclient/MsBreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msbreweryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBreweryClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
